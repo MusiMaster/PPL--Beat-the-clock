6304ed411ba44e0e49502f1b440c8c93fc5f00f6
Kevin Klar <klk46394@ei-lan-399.hs-regensburg.de>
2024-11-15 14:44:21 +0100

    both player ready fertig

--- BeatTheClock/BeatTheClock.srcs/sources_1/new/bothplayersready.vhd
+++ BeatTheClock/BeatTheClock.srcs/sources_1/new/bothplayersready.vhd
@@ -36,16 +36,20 @@ entity bothplayersready is
     port (
         p1_button, p2_button, clk   : in  std_logic;
         random_time                 : in integer; --ausgabe des prng
-        game_start                :in std_logic; -- je nach benes definition
+        game_start                  : in std_logic; -- je nach benes definition
         both_player_ready           : out std_logic
     );
 end bothplayersready ; 
 
 architecture driver of bothplayersready is
 
-signal timeout_value    : integer := 15; -- timeout = 15 sek -> spiel vorbei
-signal timeout          : integer := 0;
+signal timeout_value    : integer := 15; -- timeout = 15 sek 
+signal timeout          : integer := 0;  -- timeout = 15 sek + random_time
 signal timeout_true     : boolean := false; 
+constant clk_frequency  : integer := 100_000_000; --100Mhz
+signal timeout_cycles   : integer :=0;  -- anzahl der takte für timeout
+signal cycle_count      : integer :=0;  --zählt die takte
+signal timer_active     : boolean := false; 
 
 signal p1_ready         : boolean := false; 
 signal p2_ready         : boolean := false;
@@ -76,19 +80,29 @@ calculatetimeout: process(clk) -- berechne timeout
 end process;
 
 expiredtimeout: process(clk)begin --lässt timeout durchlaufen
+
     --timer soll starten bei start des spiels(bene signal) und bis timeout durchlaufen
     --wenn er abgelaufen ist dann timeout_true auf true setzen
-    --if(rising_edge(game_start))then
-        
-    
+  
+    if(rising_edge(game_start))then
+        timeout_cycles <= timeout * clk_frequency; --berechnung der Taktzyklen
+        cycle_count <= 0;
+        timer_active <= true;
+        timeout_true <= false;
+        elsif timer_active then --timer läuft
+            if cycle_count < timeout_cycles then
+                cycle_count <= cycle_count +1;
+            else
+                timeout_true <= true; --timeout erreicht
+                timer_active <= false;
+            end if;
+   end if;
 end process;
 
-checkboth: process(clk)begin
-    if(p1_ready and p2_ready)then -- check ob beide gedrückt habeb
+check_state: process(clk)begin
+    if((p1_ready and p2_ready) or timeout_true)then --check ob zeit abgelofen ist oder ob beide spieler bereit sind
         ready <= '1';
-        elsif((p1_ready or p2_ready) and timeout_true)then --check ob zeit abgelofen ist = timeout 
-            ready <= '1';
-        end if;
+    end if;
 end process;
 
     both_player_ready <= ready;
